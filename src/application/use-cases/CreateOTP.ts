d94b4fa0faca9d19f6a77efd184a08738691f3b7
Robert Carvalho <robert.carvalho@educare.digital>
2025-08-16 16:42:11 -0300

    feat(use-case): :sparkles: generate OTP based on created user on db

--- src/application/use-cases/CreateOTP.ts
+++ src/application/use-cases/CreateOTP.ts
@@ -1,14 +1,26 @@
-import type { IOTPEntity } from 'src/domain/entities/OTPEntity';
 import type { IOTPRepository } from 'src/domain/repositories/IOTPRepository';
+import type { IOTPService } from 'src/domain/services/IOTPService';
 
 export interface ICreateOTP {
-  execute(email: string): Promise<IOTPEntity>;
+  execute(email: string): Promise<string>;
 }
 
 export class CreateOTP implements ICreateOTP {
-  constructor(private otpRepository: IOTPRepository) {}
+  constructor(
+    private otpRepository: IOTPRepository,
+    private totpService: IOTPService,
+  ) {}
 
-  execute(email: string): Promise<IOTPEntity> {
-    return this.otpRepository.create(email);
+  async execute(email: string): Promise<string> {
+    let user = await this.otpRepository.find(email);
+
+    if (!user) {
+      const secret = this.totpService.generateSecret();
+      user = await this.otpRepository.create(email, secret);
+    }
+
+    const code = await this.totpService.generateOTP(user.email, user.secret);
+
+    return code;
   }
 }
