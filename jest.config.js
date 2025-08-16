a19a1f88919fad09e907a1718a7c7caee79ed6f2
Robert Carvalho <robert.carvalho@educare.digital>
2025-08-16 17:08:07 -0300

    ci(tests): :wrench: create jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.(test|spec).ts'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+};
