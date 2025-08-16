d327308747d112813802fda8bd2dd44627143826
Robert Carvalho <robert.carvalho@educare.digital>
2025-08-16 16:51:22 -0300

    feat(database): :sparkles: create `PrismaOTPRepository`

---
+++ src/infraestructure/database/PrismaOTPRepository.ts
@@ -0,0 +1,24 @@
+import type { PrismaClient } from 'generated/prisma';
+import type { IOTPEntity } from 'src/domain/entities/OTPEntity';
+import type { IOTPRepository } from 'src/domain/repositories/IOTPRepository';
+
+export class PrismaOTPRepository implements IOTPRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  create(email: string, secret: string): Promise<IOTPEntity> {
+    return this.prisma.oTP.create({
+      data: {
+        email,
+        secret,
+      },
+    });
+  }
+
+  find(email: string): Promise<IOTPEntity | null> {
+    return this.prisma.oTP.findFirst({
+      where: {
+        email: email,
+      },
+    });
+  }
+}
