d6e8d8e456b986a0de936f47c15179bab403d96c
Robert Carvalho <robert.carvalho@educare.digital>
2025-08-16 16:45:39 -0300

    feat(domain): :sparkles: create `IOTPService`

---
+++ src/domain/services/IOTPService.ts
@@ -0,0 +1,5 @@
+export interface IOTPService {
+  generateOTP(email: string, secret: string): string;
+  generateSecret(): string;
+  verifyOTP(email: string, secret: string, code: string): boolean;
+}
