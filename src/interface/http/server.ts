4d3fa3a01f14a6272f6fe4314cce4147f7908097
Robert Carvalho <robert.carvalho@educare.digital>
2025-08-18 20:18:56 -0300

    feat(express): :sparkles: use `express-rate-limit` as middleware

--- src/interface/http/server.ts
+++ src/interface/http/server.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import type { IEnvConfig } from 'src/infraestructure/config/env';
 import type { ILogger } from 'src/infraestructure/config/logger';
 import { errorHandler } from './middlewares/errorHandler';
+import rateLimit from 'express-rate-limit';
 
 interface IServer {
   listen: () => void;
@@ -18,6 +19,15 @@ export class Server implements IServer {
   }
 
   initiateMiddleware() {
+    const limiter = rateLimit({
+      windowMs: 5 * 60 * 1000, // 5 minutes
+      limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+      standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+      ipv6Subnet: 52,
+    });
+
+    this.app.use(limiter);
     this.app.use(express.json());
     this.app.use('/', this.router);
     this.app.use(errorHandler);
